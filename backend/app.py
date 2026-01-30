12daa3157bdd665e3b70bd81926739f8636a9075
isujitkr <skr87981@gmail.com>
2026-01-30 17:09:20 +0530

    root api added

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from config import setup_logging
 from routes.webhook_routes import webhook_bp
@@ -9,6 +9,10 @@ def create_app():
 
     app = Flask(__name__)
     CORS(app)
+    
+    @app.route("/", methods=["GET"])
+    def root():
+        return "Api is working...", 200
 
     app.register_blueprint(webhook_bp, url_prefix="/api")
 
@@ -20,5 +24,5 @@ if __name__ == "__main__":
     app.run(
     host="0.0.0.0",
     port=int(os.getenv("PORT", 4000)),
-    debug=False
+    debug=True,
 )
